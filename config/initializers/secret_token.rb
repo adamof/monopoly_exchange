a8ff0c40e3e577f2c68c09b83c395f15b090aba6
YordanPetrov <yordanpetrow@gmail.com>
2013-04-04 16:09:25 +0100

    new rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MonopolyExchange::Application.config.secret_token = 'dbc84e0ad9a87db320a0a02968ad2ad92349bbeb310b30792c69cded29dc52d6f2c86dd6c4bb212755a59f3d42cd7fd68793bd02a4d98e5c97a65313e0cc8c8b'
